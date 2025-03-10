b69f50f4cee84ecca68c52363524fd6ea1cb19d2
Julia Anisio <julia.an.nogueira@gmail.com>
2025-03-10 13:50:39 -0300

    endpoint to return posts from a user

--- src/main/java/com/juliang/workshopmongo/resources/UserResource.java
+++ src/main/java/com/juliang/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.juliang.workshopmongo.resources;
 
+import com.juliang.workshopmongo.domain.Post;
 import com.juliang.workshopmongo.domain.User;
 import com.juliang.workshopmongo.dto.UserDTO;
 import com.juliang.workshopmongo.services.UserService;
@@ -48,6 +49,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+    @RequestMapping(value="/{id}/posts",method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 
