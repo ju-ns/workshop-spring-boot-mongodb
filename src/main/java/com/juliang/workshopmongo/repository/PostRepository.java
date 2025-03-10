8e4a2335d3f14e3451d1884925d074224d2817cc
Julia Anisio <julia.an.nogueira@gmail.com>
2025-03-10 16:54:19 -0300

    simple query with @Query

--- src/main/java/com/juliang/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/juliang/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,8 @@
 package com.juliang.workshopmongo.repository;
 
 import com.juliang.workshopmongo.domain.Post;
-import com.juliang.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +10,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/juliang/workshopmongo/services/PostService.java
+++ src/main/java/com/juliang/workshopmongo/services/PostService.java
@@ -20,7 +20,7 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
     public List<Post> findByTitle(String text){
-        return repo.findByTitleContainingIgnoreCase(text);
+        return repo.searchTitle(text);
     }
 
 
